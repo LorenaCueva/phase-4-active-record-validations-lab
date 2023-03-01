c1b63c6806488fba214ab3d244556fd0d737a39d
Lorena Cueva <lorenacueva@gmail.com>
2023-03-01 12:04:15 -0800

    commit tree a947a39893d831d4bdf2e00dca1e31f2ab1a20b3

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,17 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: {in: ['Fiction', 'Non-Fiction']}
+    validate :clickbaity
+
+
+    def clickbaity
+        phrases = ["Won't Believe", "Secret", "Top", "Guess"]
+        phrases.each do |w|
+            return true if self.title && self.title[w]
+        end
+        errors.add(:title, "Not clickbaity enough")
+        # return false
+    end 
 end
